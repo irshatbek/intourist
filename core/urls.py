0ee5b54f86ba982b2c78c4dfdd22710ce510874c
irshatbek <irshatbek@gmail.com>
2021-12-16 16:28:32 +0600

    added home page and frofile page

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import homepage, profile
+
+
+urlpatterns = [
+    path('', homepage, name='homepage'),
+    path('profile/', profile, name='profile')
+]
--- intourist/urls.py
+++ intourist/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('places/', include('places.urls'))
+    path('places/', include('places.urls')),
+    path('', include('core.urls')),
 ]
